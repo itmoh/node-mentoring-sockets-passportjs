3035d7de478ff76fc65a2cb83b4de1adbade9cac
Maksym_Tarasenko@epam.com <Maksym_Tarasenko@epam.com>
2016-03-05 15:07:15 +0200

    test sockets

--- server/auth/local.js
+++ server/auth/local.js
@@ -1,17 +1,21 @@
 var passport = require('passport')
     , LocalStrategy = require('passport-local').Strategy;
 
-passport.use(new LocalStrategy(
-    function(username, password, done) {
-        User.findOne({ username: username }, function(err, user) {
-            if (err) { return done(err); }
-            if (!user) {
-                return done(null, false, { message: 'Incorrect username.' });
-            }
-            if (!user.authenticate(password)) {
-                return done(null, false, { message: 'Incorrect password.' });
-            }
-            return done(null, user);
-        });
-    }
-));
+
+module.exports = function(User) {
+
+    passport.use(new LocalStrategy(
+        function(username, password, done) {
+            User.findOne({ name: username }, function(err, user) {
+                if (err) { return done(err); }
+                if (!user) {
+                    return done(null, false, { message: 'Incorrect username.' });
+                }
+                if (!user.authenticate(password)) {
+                    return done(null, false, { message: 'Incorrect password.' });
+                }
+                return done(null, user);
+            });
+        }
+    ));
+}
--- server/config/express.js
+++ server/config/express.js
@@ -9,6 +9,7 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser');
 var errorHandler = require('errorhandler');
+var passport = require('passport');
 
 module.exports = function (app) {
     var env = app.get('env');
@@ -16,8 +17,11 @@ module.exports = function (app) {
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
   app.use(cookieParser());
-  
-  if ('production' === env) {
+    app.use(express.static('public'));
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    if ('production' === env) {
     app.use(morgan('dev'));
   }
 
